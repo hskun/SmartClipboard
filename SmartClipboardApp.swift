77a131a3d81429886dfb20dbc1e21d3add4b4747
hskun <4716767@qq.com>
2026-06-22 15:56:52 +0800

    Add files via upload

--- SmartClipboardApp.swift
+++ SmartClipboardApp.swift
@@ -14,15 +14,15 @@ struct SmartClipboardApp: App {
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     var window: NSWindow?
-    var eventMonitor: Any?
-    
+    var eventTap: CFMachPort?
+
     func applicationDidFinishLaunching(_ notification: Notification) {
         // Set app as accessory (hides from dock, runs in background)
         NSApp.setActivationPolicy(.accessory)
-        
+
         // Create the window
         let contentView = ContentView()
-        
+
         window = NSWindow(
             contentRect: NSRect(x: 0, y: 0, width: 400, height: 500),
             styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
@@ -33,36 +33,53 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         window?.title = "SmartClipboard"
         window?.isReleasedWhenClosed = false
         window?.level = .floating // Keep on top
-        
+
         setupGlobalShortcut()
     }
-    
+
     func setupGlobalShortcut() {
-        // Register Command + Control + V (V is 9 in keycode)
-        eventMonitor = NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { [weak self] event in
-            let commandKey = event.modifierFlags.contains(.command)
-            let controlKey = event.modifierFlags.contains(.control)
-            let vKey = event.keyCode == 9 // 'V' key
-            
-            if commandKey && controlKey && vKey {
-                self?.toggleWindow()
+        // Use CGEvent tap for reliable global hotkey capture
+        let eventMask = (1 << CGEventType.keyDown.rawValue)
+
+        let callback: CGEventTapCallBack = { proxy, type, event, refcon in
+            guard let refcon = refcon else { return Unmanaged.passUnretained(event) }
+            let appDelegate = Unmanaged<AppDelegate>.fromOpaque(refcon).takeUnretainedValue()
+
+            if type == .keyDown {
+                let flags = event.flags
+                let keyCode = event.getIntegerValueField(.keyboardEventKeycode)
+
+                // Command + Control + V
+                let commandControl: CGEventFlags = [.maskCommand, .maskControl]
+                if flags.contains(commandControl) && keyCode == 9 {
+                    DispatchQueue.main.async {
+                        appDelegate.toggleWindow()
+                    }
+                    return nil // Swallow the event
+                }
             }
+
+            return Unmanaged.passUnretained(event)
         }
-        
-        // Also monitor local events if the app is focused
-        NSEvent.addLocalMonitorForEvents(matching: .keyDown) { [weak self] event in
-            let commandKey = event.modifierFlags.contains(.command)
-            let controlKey = event.modifierFlags.contains(.control)
-            let vKey = event.keyCode == 9
-            
-            if commandKey && controlKey && vKey {
-                self?.toggleWindow()
-                return nil // Swallow the event
-            }
-            return event
+
+        let selfPtr = Unmanaged.passUnretained(self).toOpaque()
+
+        eventTap = CGEvent.tapCreate(
+            tap: .cghidEventTap,
+            place: .headInsertEventTap,
+            options: .defaultTap,
+            eventsOfInterest: CGEventMask(eventMask),
+            callback: callback,
+            userInfo: selfPtr
+        )
+
+        if let eventTap = eventTap {
+            let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
+            CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+            CGEvent.tapEnable(tap: eventTap, enable: true)
         }
     }
-    
+
     func toggleWindow() {
         if let window = window {
             if window.isVisible {
